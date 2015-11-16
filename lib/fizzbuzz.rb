984f557ec051e3cd50ac10a1d930c0356d9bc9f8
askl56 <amscott@openmailbox.org>
2015-11-16 23:58:37 +0000

    basic fizzbuzz implemented

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,14 @@
+# Description
+
+# If number divided by 3 print Fizz
+# If number divided by 5 print Buzz
+# If number divisible by both 3 and 5, print Fizzbuzz
+
 class Fizzbuzz
 
   def call
 
-    (1..100).each do |n|
+    (1..500).each do |n|
 
       if n % 3 == 0 && n % 5 == 0
         puts "Fizzbuzz"
@@ -10,7 +16,10 @@ class Fizzbuzz
         puts "Fizz"
       elsif n % 5 == 0
         puts "Buzz"
+      else
+        puts n
       end
+
     end
   end
 end
